1e0fa9e207716ef74c4e3b6364c7d32cfc281c01
Ramūnas <ramunas.daukas@gmail.com>
2020-09-23 13:50:08 +0300

    Added 1 button Created event for text change

--- app/src/main/java/com/example/lab1/MainActivity.java
+++ app/src/main/java/com/example/lab1/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.lab1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView txMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.txMain = findViewById(R.id.textToModify);
+    }
+
+    public void onClickTextChng(View view) {
+        this.txMain.setText("New word");
+    }
+
+    public void onClickColorChng(View view) {
+        this.txMain.setTextColor(Color.RED);
     }
 }
